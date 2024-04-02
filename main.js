8a58dab5b310f05de1c252849aaee3b3dcfca118
monicaczerevaty <111015067+monicaczerevaty@users.noreply.github.com>
2024-04-02 11:41:58 -0300

    0204

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ contadores[0].textContent = tempoObjetivo1 - tempoAtual;
 for (let i=0; i < botoes.length; i++){
     botoes [i].onclick = function() {
         for (let j=0; j<botoes.length; j++){
-            botoes[j].classList.remove ("ativo");
+            botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
         }
         botoes [i].classList.add("ativo");
