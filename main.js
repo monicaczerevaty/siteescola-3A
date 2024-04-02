cc538e81e308389ef6a2e9100e5fdb9affa3ab0f
monicaczerevaty <111015067+monicaczerevaty@users.noreply.github.com>
2024-04-02 11:34:15 -0300

    02-042024

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const textos = document.querySelectorAll(".aba-conteudo");
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-03-04T00:00:00");
 let tempoAtual = new Date();
-contadores[0].textContent = "tempoObjetivo1 - tempoAtual";
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
 
 for (let i=0; i < botoes.length; i++){
     botoes [i].onclick = function() {
